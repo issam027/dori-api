1d97d290790b3ae779f5044385b6ee628cb19e77
issamJoobs <ijoober@gmail.com>
2026-09-23 06:54:39 +0200

    no message

--- src/core/swagger/swagger-redirect.controller.ts
+++ src/core/swagger/swagger-redirect.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
+import { Public } from '../auth/jwt-auth.guard';
 
 @Controller()
 export class SwaggerRedirectController {
+    @Public()
     @Get()
     @Redirect('/api/docs', 302)
     redirectToDocs() { }
